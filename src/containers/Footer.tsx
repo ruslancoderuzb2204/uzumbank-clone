c901ede43af05f4ebc8768d8a3691bf25c2b648d
ruslantilovov2 <ruslantilovov2@gmail.com>
2024-03-28 15:26:58 +0500

    fixed footer

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-400">
+    <footer className="bg-[#7000ff]">
       <ul className="max-w-[1300px] flex justify-between py-6 text-white mx-auto w-full px-20">
         <li>
           <h3 className="text-xl py-4">Products</h3>
@@ -40,139 +40,170 @@ const Footer = () => {
           </ul>
         </li>
         <li>
-          <h3 className="text-xl py-4">Products</h3>
+          <h3 className="text-xl py-4">Services</h3>
           <ul>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Cards
+                My home
               </Link>
             </li>
 
             <li>
               <Link href="#" className=" cursor-pointer">
-                Credits
+                UzASBO
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Deposits
+                HUMO Pay
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Acoounts
+                Transfer to requirists
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Wallets
+                Kindergarten Payments
               </Link>
             </li>
-            <Link href="#" className=" cursor-pointer">
-              Money Transfers
-            </Link>
+           
           </ul>
         </li>
         <li>
-          <h3 className="text-xl py-4">Products</h3>
+          <h3 className="text-xl py-4">About the Bank</h3>
           <ul>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Cards
+              Bank structure
+
               </Link>
             </li>
 
             <li>
               <Link href="#" className=" cursor-pointer">
-                Credits
+                Management
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Deposits
+                Bank details
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Acoounts
+                Bank Charter
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Wallets
+                State Symbols
               </Link>
             </li>
-            <Link href="#" className=" cursor-pointer">
-              Money Transfers
-            </Link>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Essential Facts
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Local Acts
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Development Strategy
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Bank Mission
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+              Documents
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+              Events
+              </Link>
+            </li>
           </ul>
         </li>
         <li>
-          <h3 className="text-xl py-4">Products</h3>
+          <h3 className="text-xl py-4">Shareholders and investors</h3>
           <ul>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Cards
+                Meeting of Shareholders
               </Link>
             </li>
 
             <li>
               <Link href="#" className=" cursor-pointer">
-                Credits
+                Auditing
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Deposits
+                Information about Dividends
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Acoounts
+                Affiliates
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Wallets
+                Emission
               </Link>
             </li>
-            <Link href="#" className=" cursor-pointer">
-              Money Transfers
-            </Link>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Business Plan
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Corporate Govarnance
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Financial Data
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className=" cursor-pointer">
+                Stocks
+              </Link>
+            </li>
           </ul>
         </li>
         <li>
-          <h3 className="text-xl py-4">Products</h3>
+          <h3 className="text-xl py-4">Email</h3>
           <ul>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Cards
+                info@uzumbank.uz
               </Link>
             </li>
 
             <li>
-              <Link href="#" className=" cursor-pointer">
-                Credits
+              <Link href="#" className="text-xl block mt-4 cursor-pointer">
+                Interactive services
               </Link>
             </li>
             <li>
               <Link href="#" className=" cursor-pointer">
-                Deposits
+                Appeals of Individuls and legal entities
               </Link>
             </li>
-            <li>
-              <Link href="#" className=" cursor-pointer">
-                Acoounts
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className=" cursor-pointer">
-                Wallets
-              </Link>
-            </li>
-            <Link href="#" className=" cursor-pointer">
-              Money Transfers
-            </Link>
+           
           </ul>
         </li>
       </ul>
